ca8e10cd77e6d79045973d0e0f5e26ddb5a08d57
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-02-11 19:26:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   else 
     min_val = name_hash.first[1]
     min_key = name_hash.first[0]
-  name_hash.each do |key, val| 
-    if val <= min_val 
+  name_hash.each do |key, value| 
+    if value <= min_val 
       min_val = value
       min_key = key
     end 
